b7809db5f8b46c98b3544f812888c773fdc3d1d2
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-07 16:34:46 +0700

    fix: typo HTML element

--- src/components/paginated-theaters.vue
+++ src/components/paginated-theaters.vue
@@ -2,11 +2,11 @@
   <div class="columns">
     <div class="column is-12">
       <article class="media">
-        <fiture class="media-left">
+        <figure class="media-left">
           <p class="image is-64x64">
             <img src="https://via.placeholder.com/64x64?text=T" alt="Theater" />
           </p>
-        </fiture>
+        </figure>
 
         <div class="media-content">
           <div class="content">
