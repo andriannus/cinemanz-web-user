8ac718d9bde3159ad860874e9c5abe6b4b341920
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-07 14:10:58 +0700

    feat: create paginated movies and theaters component

---
+++ src/components/paginated-movies.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="columns is-multiline is-mobile">
+    <div class="column is-one-third is-one-third-mobile has-text-centered">
+      <img src="https://via.placeholder.com/280x420?text=No+Picture" alt="Tes">
+
+      <h6 class="is-size-6">
+        Judul
+      </h6>
+    </div>
+
+    <div class="column is-one-third is-one-third-mobile has-text-centered">
+      <img src="https://via.placeholder.com/280x420?text=No+Picture" alt="Tes">
+
+      <h6 class="is-size-6">
+        Judul
+      </h6>
+    </div>
+
+    <div class="column is-one-third is-one-third-mobile has-text-centered">
+      <img src="https://via.placeholder.com/280x420?text=No+Picture" alt="Tes">
+
+      <h6 class="is-size-6">
+        Judul
+      </h6>
+    </div>
+
+    <div class="column is-one-third is-one-third-mobile has-text-centered">
+      <img src="https://via.placeholder.com/280x420?text=No+Picture" alt="Tes">
+
+      <h6 class="is-size-6">
+        Judul
+      </h6>
+    </div>
+  </div>
+</template>
---
+++ src/components/paginated-theaters.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="columns">
+    <div class="column is-12">
+      <article class="media">
+        <fiture class="media-left">
+          <p class="image is-64x64">
+            <img src="https://via.placeholder.com/64x64?text=T" alt="Theater" />
+          </p>
+        </fiture>
+
+        <div class="media-content">
+          <div class="content">
+            <p>Nama Theater</p>
+          </div>
+        </div>
+      </article>
+    </div>
+  </div>
+</template>
