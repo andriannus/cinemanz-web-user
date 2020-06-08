a5c6edfe21812286e1df8167477bfb264a534a03
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-08 11:22:48 +0700

    improvement: dynamic content from props

--- src/components/paginated-movies.vue
+++ src/components/paginated-movies.vue
@@ -1,35 +1,26 @@
 <template>
   <div class="columns is-multiline is-mobile">
-    <div class="column is-one-third is-one-third-mobile has-text-centered">
-      <img src="https://via.placeholder.com/280x420?text=No+Picture" alt="Tes">
+    <div
+      v-for="(movie, index) in movies"
+      :key="'m-' + index"
+      class="column is-one-third is-one-third-mobile has-text-centered"
+    >
+      <img src="https://via.placeholder.com/280x420?text=No+Picture" :alt="movie.title" />
 
-      <h6 class="is-size-6">
-        Judul
-      </h6>
-    </div>
-
-    <div class="column is-one-third is-one-third-mobile has-text-centered">
-      <img src="https://via.placeholder.com/280x420?text=No+Picture" alt="Tes">
-
-      <h6 class="is-size-6">
-        Judul
-      </h6>
-    </div>
-
-    <div class="column is-one-third is-one-third-mobile has-text-centered">
-      <img src="https://via.placeholder.com/280x420?text=No+Picture" alt="Tes">
-
-      <h6 class="is-size-6">
-        Judul
-      </h6>
-    </div>
-
-    <div class="column is-one-third is-one-third-mobile has-text-centered">
-      <img src="https://via.placeholder.com/280x420?text=No+Picture" alt="Tes">
-
-      <h6 class="is-size-6">
-        Judul
-      </h6>
+      <h6 class="is-size-6">{{ movie.title }}</h6>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    movies: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
