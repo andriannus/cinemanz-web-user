61c603ca1b9d3aa653d634db439e0dee30d71c50
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-08 22:22:10 +0700

    improvement: set dynamic value for GraphQL URI

--- src/plugins/vue-apollo.plugin.js
+++ src/plugins/vue-apollo.plugin.js
@@ -4,10 +4,12 @@ import { createHttpLink } from 'apollo-link-http';
 import Vue from 'vue';
 import VueApollo from 'vue-apollo';
 
+import GRAPHQL_URI from '@/shared/constants/config.constant';
+
 Vue.use(VueApollo);
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:7777/graphql',
+  uri: process.env.NODE_ENV === 'production' ? GRAPHQL_URI.prod : GRAPHQL_URI.dev,
 });
 
 const cache = new InMemoryCache();
---
+++ src/shared/constants/config.constant.js
@@ -0,0 +1,6 @@
+const GRAPHQL_URI = {
+  dev: 'http://localhost:7777/graphql',
+  prod: 'https://cinemanz-graphql.herokuapp.com/graphql',
+};
+
+export { GRAPHQL_URI as default };
