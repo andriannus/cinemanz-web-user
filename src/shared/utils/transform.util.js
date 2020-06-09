94eba70dac05dc22aca8ebd0f54f768536f5ff05
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-09 10:21:24 +0700

    feat: create util for return paginated data

---
+++ src/shared/utils/transform.util.js
@@ -0,0 +1,16 @@
+function paginate(data, options) {
+  const { page, limit, total } = options;
+  const totalPage = Math.ceil(total / limit);
+
+  return {
+    data,
+    page,
+    perPage: limit,
+    prevPage: page - 1 ? page - 1 : null,
+    nextPage: totalPage > page ? page + 1 : null,
+    total,
+    totalPage,
+  };
+}
+
+export { paginate as default };
