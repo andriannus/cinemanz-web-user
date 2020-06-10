2cc446476cba639194cd7cb62aa649e88843244f
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-10 11:27:19 +0700

    feat: fetch and paginate theaters at home page

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -3,6 +3,8 @@ import PaginatedMovies from '@/shared/components/paginated-movies.vue';
 import PaginatedTheaters from '@/shared/components/paginated-theaters.vue';
 import PER_PAGE from '@/shared/constants/data.constant';
 import MOVIES from '@/shared/graphql/Movies.gql';
+import THEATERS from '@/shared/graphql/Theaters.gql';
+import paginate from '@/shared/utils/transform.util';
 
 export default {
   components: {
@@ -18,11 +20,17 @@ export default {
         isLoading: false,
         results: [],
       },
+      theater: {
+        errorMessage: '',
+        isLoading: false,
+        result: {},
+      },
     };
   },
 
   mounted() {
     this.fetchAllMovie();
+    this.fetchTheaters();
   },
 
   methods: {
@@ -52,6 +60,42 @@ export default {
           showing,
         },
       });
+    },
+
+    async fetchTheaters(page) {
+      const validPage = page || 1;
+      const skip = (validPage - 1) * PER_PAGE;
+
+      this.theater.isLoading = true;
+
+      try {
+        const { data } = await this.$apollo.query({
+          query: THEATERS,
+          variables: {
+            skip,
+            limit: PER_PAGE,
+          },
+        });
+
+        const { results, total } = data.theaters;
+
+        const options = { page: validPage, limit: PER_PAGE, total };
+        const result = paginate(results, options);
+
+        this.theater = {
+          ...this.theater,
+          errorMessage: '',
+          result,
+        };
+      } catch ({ networkError }) {
+        this.theater = {
+          ...this.theater,
+          errorMessage: networkError,
+          result: {},
+        };
+      } finally {
+        this.theater.isLoading = false;
+      }
     },
   },
 };
