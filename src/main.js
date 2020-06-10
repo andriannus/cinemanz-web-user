6ee48caf865a391f181007ebdf2d2c828963dcc9
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-10 10:26:26 +0700

    chore: remove unused files

--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
 import Vue from 'vue';
 
 import App from '@/App.vue';
+import '@/main.scss';
 import router from '@/router';
-import store from '@/store';
 import apolloProvider from '@/plugins/vue-apollo.plugin';
 import '@/plugins/vue-meta.plugin';
-import '@/main.scss';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   apolloProvider,
   render: (h) => h(App),
 }).$mount('#app');
