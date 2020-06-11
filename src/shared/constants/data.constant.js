2952482344230842bd429f80c63633357099445c
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-11 09:03:56 +0700

    feat: create constant for image urls

--- src/shared/constants/data.constant.js
+++ src/shared/constants/data.constant.js
@@ -1,3 +1,7 @@
 const PER_PAGE = 6;
+const DEFAULT_IMAGE_URL = {
+  movie: 'https://via.placeholder.com/280x420?text=No+Picture',
+  theater: 'https://via.placeholder.com/64x64',
+};
 
-export { PER_PAGE as default };
+export { PER_PAGE as default, DEFAULT_IMAGE_URL };
