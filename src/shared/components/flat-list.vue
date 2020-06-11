5f7bd38290206c079c0a92dac1305c8a2c841bea
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-11 09:03:34 +0700

    feat: create new component

---
+++ src/shared/components/flat-list.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <span v-if="!words.length">-</span>
+
+    <span
+      v-else
+      v-for="(word, index) in words"
+      :key="'fl-' + index"
+      class="is-capitalized"
+    >{{ word + (index + 1 === words.length ? '' : ', ') }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    words: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
