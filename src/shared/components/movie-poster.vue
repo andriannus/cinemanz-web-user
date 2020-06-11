8c6f7e1a02528dbb663960f622ad67d5aafcaf1e
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-11 09:15:16 +0700

    feat: create new component

---
+++ src/shared/components/movie-poster.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <img :src="getValidImgUrl(src)" :alt="alt" @error="handleLoadImageError" />
+  </div>
+</template>
+
+<script>
+import { DEFAULT_IMAGE_URL } from '@/shared/constants/data.constant';
+
+export default {
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+    alt: {
+      type: String,
+      default: '',
+    },
+  },
+
+  methods: {
+    getValidImgUrl(url) {
+      const validUrl = url || DEFAULT_IMAGE_URL.movie;
+
+      return validUrl;
+    },
+
+    handleLoadImageError(e) {
+      e.target.src = DEFAULT_IMAGE_URL.movie;
+    },
+  },
+};
+</script>
