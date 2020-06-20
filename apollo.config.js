7debef11234678504584c1d00c4127b24e71cc32
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-20 20:40:12 +0700

    chore: setup GraphQL URL at Apollo config

--- apollo.config.js
+++ apollo.config.js
@@ -1,9 +1,14 @@
+const GRAPHQL_URL = {
+  dev: 'http://localhost:7777/graphql',
+  prod: 'https://cinemanz-graphql.herokuapp.com/graphql',
+};
+
 module.exports = {
   client: {
     service: {
       name: 'my-app',
       // URL to the GraphQL API
-      url: 'http://localhost:7777/graphql',
+      url: process.env.NODE_ENV === 'production' ? GRAPHQL_URL.prod : GRAPHQL_URL.dev,
     },
     // Files processed by the extension
     includes: ['src/**/*.vue', 'src/**/*.js'],
