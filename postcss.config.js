c2a647f4b44601e7df7224187e1bd61cb2bcc7a9
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-20 19:55:31 +0700

    feat: use purgecss

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+const IN_PRODUCTION = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  plugins: [
+    IN_PRODUCTION && purgecss({
+      content: ['./public/**/*.html', './src/**/*.vue'],
+      defaultExtractor(content) {
+        const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
+        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
+      },
+      whitelist: [],
+      whitelistPatterns: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/],
+    }),
+  ],
+};
